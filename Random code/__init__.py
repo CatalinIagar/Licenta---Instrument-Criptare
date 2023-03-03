2d71d7c36b7c8988364c70b41b16c834ac111c9b
CatalinIagar <cataliniagar2000@gmail.com>
2023-03-03 13:33:57 +0200

    aaa

--- Random code/__init__.py
+++ Random code/__init__.py
@@ -3,3 +3,6 @@ import os
 file = "C:/Users/catal/PycharmProjects/licenta/main.py.aes"
 extension = os.path.splitext(file)[0]
 print(extension)
+
+with open(filename, 'rb') as file:
+    content = file.read()
--- main.py
+++ main.py
@@ -1,26 +1,47 @@
 import sys
+import os
+
+import printHelper
+
+def printFileDetails(file):
+    filename = os.path.splitext(file)[0]
+    extension = os.path.splitext(file)[1]
+    size = os.path.getsize(file)
+    print("File name: " + filename)
+    print("File extension: " + extension)
+    print("File size: " + str(size) + " bytes")
 
 
 def encryptionStart():
     print("Encryption process")
 
     if len(sys.argv) < 4:
-        print("Missing arguments")
-        printHelp()
+        print("Use -f to specify the file for encryption")
         return
+
+    if len(sys.argv) < 6:
+        print("Use -alg to specify the encryption algorithm")
+        print("Use -h -alg to see the list of available algorithms")
+        return
+
+    filename = sys.argv[3]
+    algorithm = sys.argv[5]
+
+    if algorithm == "AES":
+        print("AES")
+    elif algorithm == "TwoFish":
+        print("TwoFish")
+    elif algorithm == "Salsa":
+        print("Salsa 20/12")
+    elif algorithm == "RC4":
+        print("RC4")
     else:
-        print(sys.argv[3])
+        print("Invalid algorithm")
+        print("Use -h -alg to see the list of available algorithms")
+        return
 
 def decryptionStart():
     print("Decryption process")
-
-
-def printHelp():
-    print("First argument:")
-    print("    encrypt - for encryption mode")
-    print("    decrypt - for decryption mode")
-    print("2nd argument:")
-    print("    -f - specify the file you want to encrypt/decrypt")
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
@@ -29,8 +50,12 @@ if __name__ == '__main__':
         elif sys.argv[1] == "decrypt":
             decryptionStart()
         elif sys.argv[1] == "-h":
-            printHelp()
+            if len(sys.argv) == 3:
+                extraArg = sys.argv[2]
+                printHelper.printHelpExtra(extraArg)
+            else:
+                printHelper.printHelp()
         else:
-            printHelp()
+            printHelper.printHelp()
     else:
-        printHelp()
+        printHelper.printHelp()
