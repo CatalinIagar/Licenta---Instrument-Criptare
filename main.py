5f251541145720b0b4dd7b80171b6ea6d86d0beb
CatalinIagar <cataliniagar2000@gmail.com>
2023-03-28 15:52:29 +0300

    turned AES into a class

--- main.py
+++ main.py
@@ -2,6 +2,54 @@ import sys
 import os
 
 import printHelper
+from Algorithms import AES
+
+ALGORITHMS = ["AES", "TwoFish", "Salsa", "RC4", "RSA", "ECC"]
+AES_MODES = ["ECB", "CBC", "CFB", "OFB"]
+
+def findParam(param):
+    found = False
+    pos = -1
+    for i in range(1, len(sys.argv)):
+        if param == sys.argv[i]:
+            found = True
+            pos = i
+
+    if not found:
+        print("Argument {} not found.".format(param))
+        return False
+
+    if (pos + 1) > len(sys.argv) - 1:
+        print("Missing value for {}".format(param))
+        return False
+
+    value = sys.argv[pos + 1]
+
+    if param == "-f":
+        if os.path.isfile(value):
+            return value
+        else:
+            print("File doesn't exist")
+            return False
+
+    if param == "-alg":
+        if value in ALGORITHMS:
+            return value
+        else:
+            print("Invalid algorithm")
+            print("Use -h -alg to see the list of available algorithms")
+            return False
+
+    if param == "-m":
+        if value in AES_MODES:
+            return value
+        else:
+            print("Invalid mode")
+            print("Use -h -alg to see the available modes of AES")
+            return False
+
+    if param == "-k":
+        return value
 
 def printFileDetails(file):
     filename = os.path.splitext(file)[0]
@@ -12,24 +60,33 @@ def printFileDetails(file):
     print("File size: " + str(size) + " bytes")
 
 def encryptAES(file):
-    if len(sys.argv) < 8 or sys.argv[6] != "-m":
-        print("Use -m to specify AES mode")
+    mode = findParam("-m")
+    if not mode:
+        print("Use -m to specify the mode for AES")
         return
 
-    mode = sys.argv[7]
+    key = findParam("-k")
 
-    if len(sys.argv) < 10 or sys.argv[8] != "-k":
-        print("Use -k to specify encryption key")
+    if not key:
+        print("Use -k to specify the mode for AES")
         return
 
-    key = sys.argv[9]
     if len(key) != 16:
         print("Invalid key")
         print("Key must be 128-bit long (16 characters)")
         return
 
     if mode == "ECB":
-        print("ECB")
+        printFileDetails(file)
+        print("Algorithm: AES")
+        print("Mode: EBC")
+        print("Key: {}".format(key))
+        res = input("Continue? (y/n): ")
+        if res == "y":
+            encryption = AES.AES(file, "ECB", key)
+            encryption.encrypt()
+        else:
+            print("Program stopped")
     elif mode == "CBC":
         print("CBC")
     elif mode == "CFB":
@@ -43,17 +100,16 @@ def encryptAES(file):
 def encryptionStart():
     print("Encryption process")
 
-    if len(sys.argv) < 4 or sys.argv[2] != "-f":
+    filename = findParam("-f")
+    if not filename:
         print("Use -f to specify the file for encryption")
         return
 
-    if len(sys.argv) < 6 or sys.argv[4] != "-alg":
+    algorithm = findParam("-alg")
+    if not algorithm:
         print("Use -alg to specify the encryption algorithm")
         print("Use -h -alg to see the list of available algorithms")
         return
-
-    filename = sys.argv[3]
-    algorithm = sys.argv[5]
 
     if algorithm == "AES":
         encryptAES(filename)
